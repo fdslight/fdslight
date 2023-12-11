d1ef19b1010f54fe4e0492c7592b01761c30174d
freek <freekai@outlook.com>
2023-12-11 21:48:52 +0800

    ...

--- fdsl_client.py
+++ fdsl_client.py
@@ -954,9 +954,9 @@ class _fdslight_client(dispatcher.dispatcher):
                 self.__last_local_ip6 = byte_addr
             else:
                 self.__last_local_ip = byte_addr
-
+            print("A", netpkt)
             racs_cext.modify_ip_address_from_netpkt(netpkt, rewrite_local_addr, is_src, is_ipv6)
-
+            print("B", netpkt)
             return netpkt
 
         if is_ipv6:
