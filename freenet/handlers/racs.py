cd11485f09579613fedde1e79c2b8a7e7f898486
freek <freekai@outlook.com>
2025-06-15 21:50:18 +0800

    ...

--- freenet/handlers/racs.py
+++ freenet/handlers/racs.py
@@ -201,6 +201,7 @@ class tcp_tunnel(tcp_handler.tcp_handler):
 
         self.writer.write(wrap_data)
         self.add_evt_write(self.fileno)
+        self.send_now()
 
     def send_heartbeat(self):
         self.send_msg(b"")
