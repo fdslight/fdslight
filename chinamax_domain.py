bc28d3dc181a92ee6c9194993653f0bfe397ac69
mac <freekai@outlook.com>
2026-08-29 11:49:56 +0800

    ...

--- chinamax_domain.py
+++ chinamax_domain.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 # https://github.com/blackmatrix7/ios_rule_script/blob/master/rule/Clash/ChinaMax/ChinaMax_Domain.txt
+# 生成规则文件
 
-import subprocess, os
+import subprocess, os, time
 
 
 def generate(src_path, dst_path, adds: list, suffix):
+    up_time = time.strftime("# %Y-%m-%d %H:%M:%S %Z\n")
     dst = open(dst_path, "w")
+    dst.write(up_time)
+    s = """
+### --------------重要说明--------------------------
+## 域名前缀的"*"表示匹配所有,"#"开头的表示注释
+# ":0" 表示DNS走加密
+# ":1" 表示解析DNS并且走代理
+# ":2"表示丢弃DNS请求,可用于广告屏蔽等其他用途
+# ":3"表示域名所对应的IP地址不走代理
+
+###-------------------------------------------------
+"""
+    dst.write(s)
+    dst.write("\n")
+
     for name, value in adds:
         dst.write(name + ":" + value + "\n")
+    dst.write("\n")
     src = open(src_path, 'r')
     for line in src:
         line = line.strip()
@@ -35,7 +52,10 @@ def main():
     url = "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Clash/ChinaMax/ChinaMax_Domain.txt"
 
     subprocess.call("curl %s -o chinamax_domain.txt" % url, shell=True)
-    generate("chinamax_domain.txt", "host_rules.txt", [("*", "1")], "3")
+    adds = [
+        ("*", "1"),
+    ]
+    generate("chinamax_domain.txt", "host_rules.txt", adds, "3")
 
 
 if __name__ == "__main__":
