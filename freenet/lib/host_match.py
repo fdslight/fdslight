b5d82d132b2a4c32db5944bb5edd1d93ce9d6ceb
mac <freekai@outlook.com>
2026-08-30 17:34:48 +0800

    ...

--- freenet/lib/host_match.py
+++ freenet/lib/host_match.py
@@ -21,12 +21,14 @@ class host_match(object):
 
         o = self.__rule_tree
         is_found = True
+        i = 0
         for x in _list:
             if x in self.__internal_keywords: x = "__%s" % x
             if x not in o:
                 is_found = False
                 break
             o = o[x]
+            i += 1
 
         if is_found:
             # 这里可能存在xxx.xx格式的域名情况,需要额外考虑
@@ -34,20 +36,24 @@ class host_match(object):
             if o["rule_info"]:
                 return True, o["rule_info"]["action"]
             # 补全一个空元素,以便join方法形成".xxx.xx"域名
-            _list.append("")
-            _list.reverse()
-            return self.match(".".join(_list))
+            # _list.append("")
+            # _list.reverse()
+            # return self.match(".".join(_list))
 
         # 未匹配那么查找子项所有匹配是否存在
         if "*" in o:
             rs = o["*"]["rule_info"]["action"]
             return True, rs
 
-        o = self.__rule_tree
-        if "*" in o:
-            return True, o["*"]["rule_info"]["action"]
+        # 子项不存在那么逐渐减少域名等级域,执行最大化匹配
+        if i == 0: return None
+        i -= 1
+        new_list = _list[0:i]
+        new_list.append("")
+        new_list.reverse()
+        # print(new_list)
 
-        return False, None
+        return self.match(".".join(new_list))
 
     def add_rule(self, rule_object: tuple):
         """加入规则
